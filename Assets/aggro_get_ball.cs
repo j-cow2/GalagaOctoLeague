56a11f73cd59d818f3f044451133cb97c65f6ff5
plorkin <jfournier989@gmail.com>
2025-02-28 23:28:55 -0600

    Adultery

--- Assets/aggro_get_ball.cs
+++ Assets/aggro_get_ball.cs
@@ -2,31 +2,49 @@ using UnityEngine;
 
 public class aggro_get_ball : MonoBehaviour
 {
-    public GameObject ball;
     public float aggroDistance = 5;
-    public rigidbody2D rb;
-    public float speed = 5;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Rigidbody2D rb;
+    public float speed = 3;
+    private GameObject ball;
+    private Vector2 origin;
+    
+
+
+    private void Start()
     {
-        
+        spawner spawnerScript = FindObjectOfType<spawner>();
+        if (spawnerScript != null)
+        {
+            ball = spawnerScript.target_ball;
+        }
+        origin = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float distanceToBall = Vector2.Distance(transform.position, ball.transform.position);
-        if (distanceToBall <= aggroDistance)
+        if (ball != null) 
         {
-        Vector2 direction = (ball.transform.position - transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        rb.velocity = direction * speed;
+            float distanceToBall = Vector2.Distance(transform.position, ball.transform.position);
+            if (distanceToBall <= aggroDistance)
+            {
+                Vector2 direction = (ball.transform.position - transform.position).normalized;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rb.rotation = angle - 90;
+                rb.linearVelocity = direction * speed;
+            }
+            else if (Vector2.Distance(transform.position, origin) > 0.1f)
+            {
+                Vector2 direction = (origin - (Vector2)transform.position).normalized;
+                rb.linearVelocity = direction * speed;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rb.rotation = angle - 90;
+            }
+            else
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.rotation = 0;
+            }
         }
-        else
-        {
-        rb.velocity = Vector2.zero;
-        }
-    
     }
 }
